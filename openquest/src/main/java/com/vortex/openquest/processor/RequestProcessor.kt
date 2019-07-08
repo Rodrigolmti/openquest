c2c2771897e9c78b07bc450fb37c05d91c68c1e1
Rodrigo Lopes <rodrigolopes@ioasys.com.br>
2019-07-08 15:10:33 -0300

    Add serialize body in adapter

--- openquest/src/main/java/com/vortex/openquest/processor/RequestProcessor.kt
+++ openquest/src/main/java/com/vortex/openquest/processor/RequestProcessor.kt
@@ -1,6 +1,29 @@
 package com.vortex.openquest.processor
 
-import com.google.gson.GsonBuilder
+/*
+* MIT License
+*
+* Copyright (c) 2019 Rodrigo Lopes Martins
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 import com.vortex.openquest.contracts.RequestCommand
 import com.vortex.openquest.util.Builder
 import com.vortex.openquest.util.Error
@@ -8,7 +31,6 @@ import com.vortex.openquest.util.Response
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.IOException
-import java.io.InputStream
 import java.net.HttpURLConnection
 import javax.net.ssl.HttpsURLConnection
 import kotlin.coroutines.resume
@@ -40,24 +62,25 @@ internal class RequestProcessor(
                     }
                 }
 
-                requestCommand.builder.requestBody?.let { body ->
-                    connection.doOutput = true
-                    val json = GsonBuilder().create().toJson(body)
-                    connection.outputStream.use { outputStream ->
-                        val input = json.toByteArray(Charsets.UTF_8)
-                        outputStream.write(input)
-                    }
-                }
-
-                connection.connect()
-
                 requestCommand.converterAdapter?.let { converter ->
+                    requestCommand.builder.requestBody?.let { body ->
+                        connection.doOutput = true
+                        connection.outputStream.use { outputStream ->
+                            outputStream.write(converter.serializeBody(body))
+                        }
+                    }
+
+                    connection.connect()
+
                     val response = converter.serializeResponse<R>(connection.inputStream)
                     response?.let {
                         continuation.resume(Response.Success(connection.responseCode, response))
                     } ?: run {
                         continuation.resume(Response.Failure(Error.Unknown))
                     }
+
+                } ?: run {
+                    continuation.resume(Response.Failure(Error.InvalidConverterAdpter))
                 }
 
             } catch (error: IOException) {
